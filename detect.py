483511fdea8c3489b903540bd98f1a1d7eb3db96
Elad P <eladpar@gmail.com>
2022-12-18 14:39:47 +0200

    added checks

--- detect.py
+++ detect.py
@@ -229,9 +229,10 @@ def run(
                         vid_writer[i].write(im0)
 
         # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        import time
+        LOGGER.info(f"{time.time()}s}{'' if len(det) else '(no detections), '}{(dt[1].dt +dt[0].dt + dt[2].dt) * 1E3:.1f}ms"),
         # Print time (post process-only)
-        LOGGER.info(f"post process time is{'' if len(det) else '(no detections), '}{dt_post.dt * 1E3:.1f}ms")
+        LOGGER.info(f"post process time is {dt_post.dt * 1E3:.1f}ms")
 
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
