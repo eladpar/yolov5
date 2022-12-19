18b79cee2cecd2064ed616cabb87a0d1f57a6252
Elad P <eladpar@gmail.com>
2022-12-19 10:20:51 +0200

    added process timing

--- detect.py
+++ detect.py
@@ -229,10 +229,10 @@ def run(
                             vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                         vid_writer[i].write(im0)
 
-        # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
-        # Print time (post process-only)
-        LOGGER.info(f"post process time is{'' if len(det) else '(no detections), '}{dt_post.dt * 1E3:.1f}ms")
+        # Print time 
+        import time
+        LOGGER.info(f"{time.time()}{s}{'' if len(det) else '(no detections), '}{(dt[1].dt +dt[0].dt + dt[2].dt) * 1E3:.1f}ms post process time is {dt_post.dt * 1E3:.1f}ms"),
+
 
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
